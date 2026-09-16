ffd2ee9f3041b3f55bff3ba1087aad0ad1d99859
vectechcb-hue <vectech.cb@gmail.com>
2026-09-16 15:53:46 +0800

    fix PWA cache update and activate immediately

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
-const CACHE='product-issue-tracker-v3';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL))));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+const CACHE='product-issue-tracker-v4';
+const STATIC=['./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)));
+});
+self.addEventListener('activate',event=>event.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
-  event.respondWith(
-    fetch(event.request).then(response=>{
-      if(response.ok && new URL(event.request.url).origin===location.origin){
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      }
-      return response;
-    }).catch(()=>caches.match(event.request).then(cached=>cached || caches.match('./index.html')))
-  );
+  const url=new URL(event.request.url);
+  if(url.origin!==location.origin){return;}
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(fetch(event.request).then(response=>{
+    if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));}
+    return response;
+  }).catch(()=>caches.match(event.request)));
 });
