84e37e0a10c8d01972cad41f18f25c99828b254d
vectechcb-hue <vectech.cb@gmail.com>
2026-09-16 17:07:01 +0800

    Bump PWA cache for Firebase auth fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='product-issue-tracker-v7';
+const CACHE='product-issue-tracker-v8';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
